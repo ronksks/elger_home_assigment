5a65b678f1474fa8731cb9f01d45c2243d1a23fa
Ron Yalensky <ron.yalensky@gmail.com>
2023-01-24 15:24:15 +0200

    q4 recursive numeric sequencer solution

--- q4_recursive_numeric_sequencer/q4_recursive_numeric_sequencer.py
+++ q4_recursive_numeric_sequencer/q4_recursive_numeric_sequencer.py
@@ -1,7 +1,7 @@
 def recursive_numeric_sequencer(num, max_num, max_num_cnt):
     if num == 0:
         print(f' The max num is: {max_num} and it appears: {max_num_cnt} times')
-        return 1
+        return
     if num == max_num:
         max_num_cnt += 1
     if num > max_num:
